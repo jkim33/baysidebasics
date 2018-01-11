0c3015c828f8151a8061ab450269c3a505f09e2d
Derek <Derek>
2018-01-11 08:38:12 -0500

    BeginnerAI work

--- BeginnerAI.java
+++ BeginnerAI.java
@@ -12,14 +12,453 @@ public class BeginnerAI extends Player {
 	}
     }
 
-    public void placeCruiser() {
+    public void placeCarrier() {
+	int row = (int) (Math.random() * 10);
+	int col = (int) (Math.random() * 10);
+	int orientation = (int) (Math.random() * 4) + 1;
+	int holder = 4; // because starting index is 0
+
+	if (orientation == 1) {
+	    if (row < holder) {
+		placeCarrier();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row - holder][col] != "O") {
+		    placeCarrier();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 2) {
+	    if (col > 9 - holder) {
+			placeCarrier();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row][col + holder] != "O") {
+		    placeCarrier();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 3) {
+	    if (row > 9 - holder) {
+		placeCarrier();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row + holder][col] != "O") {
+		    placeCarrier();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 4) {
+	    if (col < holder) {
+		placeCarrier();
+		return;
+	    } 
+	    while (holder > -1) {
+		if (_grid[row][col - holder] != "O") {
+		    placeCarrier();
+		    return;
+		}
+		holder--;
+	    }
+	}
+
+	holder = 4;
+	if (orientation == 1) {
+	    while (holder > -1) {
+		_grid[row - holder][col] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 2) {
+	    while (holder > -1) {
+		_grid[row][col + holder] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 3) {
+	    while (holder > -1) {
+		_grid[row + holder][col] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 4) {
+	    while (holder > -1) {
+		_grid[row][col - holder] = "C";
+		holder--;
+	    }
+	}
     }
     public void placeBattleship() {
+	
+	int row = (int) (Math.random() * 10);
+	int col = (int) (Math.random() * 10);
+	int orientation = (int) (Math.random() * 4) + 1;
+	int holder = 4; // because starting index is 0
+
+	if (orientation == 1) {
+	    if (row < holder) {
+		placeBattleship();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row - holder][col] != "O") {
+		    placeBattleship();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 2) {
+	    if (col > 9 - holder) {
+		placeBattleship();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row][col + holder] != "O") {
+		    placeBattleship();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 3) {
+	    if (row > 9 - holder) {
+		placeBattleship();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row + holder][col] != "O") {
+		    placeBattleship();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 4) {
+	    if (col < holder) {
+		placeBattleship();
+		return;
+	    } 
+	    while (holder > -1) {
+		if (_grid[row][col - holder] != "O") {
+		    placeBattleship();
+		    return;
+		}
+		holder--;
+	    }
+	}
+
+	holder = 4;
+	if (orientation == 1) {
+	    while (holder > -1) {
+		_grid[row - holder][col] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 2) {
+	    while (holder > -1) {
+		_grid[row][col + holder] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 3) {
+	    while (holder > -1) {
+		_grid[row + holder][col] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 4) {
+	    while (holder > -1) {
+		_grid[row][col - holder] = "C";
+		holder--;
+	    }
+	}
     }
-    public void placeCarrier() {
+    public void placeCruiser() {
+	
+	int row = (int) (Math.random() * 10);
+	int col = (int) (Math.random() * 10);
+	int orientation = (int) (Math.random() * 4) + 1;
+	int holder = 4; // because starting index is 0
+
+	if (orientation == 1) {
+	    if (row < holder) {
+		placeCruiser();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row - holder][col] != "O") {
+		    placeCruiser();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 2) {
+	    if (col > 9 - holder) {
+		placeCruiser();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row][col + holder] != "O") {
+		    placeCruiser();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 3) {
+	    if (row > 9 - holder) {
+		placeCruiser();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row + holder][col] != "O") {
+		    placeCruiser();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 4) {
+	    if (col < holder) {
+		placeCruiser();
+		return;
+	    } 
+	    while (holder > -1) {
+		if (_grid[row][col - holder] != "O") {
+		    placeCruiser();
+		    return;
+		}
+		holder--;
+	    }
+	}
+
+	holder = 4;
+	if (orientation == 1) {
+	    while (holder > -1) {
+		_grid[row - holder][col] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 2) {
+	    while (holder > -1) {
+		_grid[row][col + holder] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 3) {
+	    while (holder > -1) {
+		_grid[row + holder][col] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 4) {
+	    while (holder > -1) {
+		_grid[row][col - holder] = "C";
+		holder--;
+	    }
+	}
     }
+
+    
     public void placeSubmarine() {
+	
+	
+	int row = (int) (Math.random() * 10);
+	int col = (int) (Math.random() * 10);
+	int orientation = (int) (Math.random() * 4) + 1;
+	int holder = 4; // because starting index is 0
+
+	if (orientation == 1) {
+	    if (row < holder) {
+		placeSubmarine();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row - holder][col] != "O") {
+		    placeSubmarine();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 2) {
+	    if (col > 9 - holder) {
+	        placeSubmarine();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row][col + holder] != "O") {
+		    placeSubmarine();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 3) {
+	    if (row > 9 - holder) {
+		placeSubmarine();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row + holder][col] != "O") {
+		    placeSubmarine();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 4) {
+	    if (col < holder) {
+		placeSubmarine();
+		return;
+	    } 
+	    while (holder > -1) {
+		if (_grid[row][col - holder] != "O") {
+		 placeSubmarine();
+		    return;
+		}
+		holder--;
+	    }
+	}
+
+	holder = 4;
+	if (orientation == 1) {
+	    while (holder > -1) {
+		_grid[row - holder][col] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 2) {
+	    while (holder > -1) {
+		_grid[row][col + holder] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 3) {
+	    while (holder > -1) {
+		_grid[row + holder][col] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 4) {
+	    while (holder > -1) {
+		_grid[row][col - holder] = "C";
+		holder--;
+	    }
+	}
     }
+
+    
     public void placeDestroyer() {
+	
+	int row = (int) (Math.random() * 10);
+	int col = (int) (Math.random() * 10);
+	int orientation = (int) (Math.random() * 4) + 1;
+	int holder = 4; // because starting index is 0
+
+	if (orientation == 1) {
+	    if (row < holder) {
+		placeDestroyer();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row - holder][col] != "O") {
+		    placeDestroyer();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 2) {
+	    if (col > 9 - holder) {
+		placeDestroyer();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row][col + holder] != "O") {
+		    placeDestroyer();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 3) {
+	    if (row > 9 - holder) {
+		placeDestroyer();
+		return;
+	    }
+	    while (holder > -1) {
+		if (_grid[row + holder][col] != "O") {
+		    placeDestroyer();
+		    return;
+		}
+		holder--;
+	    }
+	}
+	
+	if (orientation == 4) {
+	    if (col < holder) {
+		placeDestroyer();
+		return;
+	    } 
+	    while (holder > -1) {
+		if (_grid[row][col - holder] != "O") {
+		    placeDestroyer();
+		    return;
+		}
+		holder--;
+	    }
+	}
+
+	holder = 4;
+	if (orientation == 1) {
+	    while (holder > -1) {
+		_grid[row - holder][col] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 2) {
+	    while (holder > -1) {
+		_grid[row][col + holder] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 3) {
+	    while (holder > -1) {
+		_grid[row + holder][col] = "C";
+		holder--;
+	    }
+	}
+	if (orientation == 4) {
+	    while (holder > -1) {
+		_grid[row][col - holder] = "C";
+		holder--;
+	    }
+	}
     }
 }
